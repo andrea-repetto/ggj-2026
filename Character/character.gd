429915ae488bf8732a7761f8207d83c92a0a0f0e
Vorblaka <200572@buas.nl>
2026-01-31 19:43:21 +0100

    Fixed character camera

--- Character/character.gd
+++ Character/character.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-@onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
+@onready var body_sprite : AnimatedSprite2D = $Body
+@onready var head_sprite : AnimatedSprite2D = $Head
 var speed : float = 300
 var knockback_velocity : Vector2 = Vector2.ZERO
 var knockback_speed : float = 1000
@@ -10,10 +11,12 @@ func _physics_process(_delta: float) -> void:
 	var animation_to_play : String
 	velocity = knockback_velocity + Vector2(Input.get_axis("walk_left", "walk_right"), Input.get_axis("walk_up", "walk_down")).normalized() * speed
 	if velocity.x < -1:
-		sprite.flip_h = true
+		body_sprite.flip_h = true
+		head_sprite.flip_h = true
 		animation_to_play = "WALK_SIDE"
 	if velocity.x > 1:
-		sprite.flip_h = false
+		body_sprite.flip_h = false
+		head_sprite.flip_h = false
 		animation_to_play = "WALK_SIDE"
 	if velocity == Vector2.ZERO :
 		animation_to_play = "IDLE"
@@ -21,7 +24,8 @@ func _physics_process(_delta: float) -> void:
 		animation_to_play = "WALK_UP"
 	if velocity.y < -1 :
 		animation_to_play = "WALK_DOWN"
-	sprite.play(animation_to_play)
+	body_sprite.play(animation_to_play)
+	head_sprite.play(animation_to_play)
 	move_and_slide()
 	if knockback_velocity.length() > 0:
 		knockback_velocity = knockback_velocity.move_toward(Vector2.ZERO, knockback_deceleration)
