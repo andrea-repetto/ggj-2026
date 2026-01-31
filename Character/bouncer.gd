a9d7fd3cd41c38150c9389e02fcb0003789965e0
Andrea Repetto <andrea.repetto85@gmail.com>
2026-01-31 18:18:34 +0100

    Rename tension_increase to update_tension

--- Character/bouncer.gd
+++ Character/bouncer.gd
@@ -1,7 +1,8 @@
 extends Node2D
-var tension_on_bounce = 10.0
+
+var tension_on_bounce : float = 10.0
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Bounceable") :
 		body.knockback(position)
-		Global.tension_increase(10)
+		Global.update_tension(tension_on_bounce)
--- scripts/game_instance.gd
+++ scripts/game_instance.gd
@@ -11,13 +11,13 @@ var nav_region_RID: RID
 signal guest_added(guest: Guest)
 signal guest_removed(guest: Guest)
 signal guest_changed_state(guest: Guest, new_state: Guest.GuestState)
-signal increase_tension(total : float)
+signal tension_changed(total : float)
 
 func _ready() -> void:
 	var nav_regions := get_tree().get_nodes_in_group("nav_region")
 	if not nav_regions.is_empty():
 		nav_region_RID = nav_regions[0].get_rid()
 
-func tension_increase(amount : float):
+func update_tension(amount : float):
 	current_tension = clamp(current_tension-amount, 0, max_tension)
-	increase_tension.emit(current_tension)
+	tension_changed.emit(current_tension)
