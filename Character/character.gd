1ab356f2eb8c3af2287560e395b88d42ec8da6ac
Vorblaka <200572@buas.nl>
2026-01-31 11:24:58 +0100

    Character and bouncers area

---
+++ Character/character.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+@onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
+var speed : float = 300
+var knockback_velocity : Vector2 = Vector2.ZERO
+var knockback_speed : float = 1000
+var knockback_deceleration : float = 10
+
+func _physics_process(_delta: float) -> void:
+	var animation_to_play : String
+	velocity = knockback_velocity + Vector2(Input.get_axis("walk_left", "walk_right"), Input.get_axis("walk_up", "walk_down")).normalized() * speed
+	if velocity.x < -1:
+		sprite.flip_h = true
+		animation_to_play = "WALK_SIDE"
+	if velocity.x > 1:
+		sprite.flip_h = false
+		animation_to_play = "WALK_SIDE"
+	if velocity == Vector2.ZERO :
+		animation_to_play = "IDLE"
+	if velocity.y > 1 :
+		animation_to_play = "WALK_UP"
+	if velocity.y < -1 :
+		animation_to_play = "WALK_DOWN"
+	sprite.play(animation_to_play)
+	move_and_slide()
+	if knockback_velocity.length() > 0:
+		knockback_velocity = knockback_velocity.move_toward(Vector2.ZERO, knockback_deceleration)
+		print(knockback_velocity)
+
+func knockback(from : Vector2):
+	var dir : Vector2 = (position - from).normalized()
+	knockback_velocity = dir * knockback_speed
