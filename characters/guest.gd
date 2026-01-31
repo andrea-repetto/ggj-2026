3270e67c8b41d395685b8b81a83241e424799e83
Martiriak <51883953+Martiriak@users.noreply.github.com>
2026-01-31 13:01:35 +0100

    Made the guest move randomly.

--- characters/guest.gd
+++ characters/guest.gd
@@ -2,6 +2,9 @@ class_name Guest
 extends CharacterBody2D
 
 
+const DISTANCE_THRESHOLD_SQUARED := 10.0 * 10.0
+
+
 enum GuestState {
 	SEEKING_PARTNER,
 	GETTING_TO_DANCE_POSITION,
@@ -11,9 +14,17 @@ enum GuestState {
 }
 
 
+## Pixel per seconds.
+@export_range(0.1, 1.0, 0.1, "or_greater", "hide_slider")
+var speed: float = 10.0
+
 var current_state := GuestState.SEEKING_PARTNER
 
 var _dance_partner: Node2D = null
+var _movement_target := Vector2(
+	randf_range(0.0, 500.0),
+	randf_range(0.0, 500.0)
+)
 
 
 func is_available() -> bool:
@@ -28,13 +39,18 @@ func set_partner(dance_partner: Node2D) -> bool:
 	return false
 
 
+func _init() -> void:
+	motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
+
+
 func _process(delta: float) -> void:
-	if Input.is_key_pressed(KEY_SPACE):
-		current_state = (current_state + 1) % (GuestState.DEAD + 1)
-	
 	match current_state:
 		GuestState.SEEKING_PARTNER:
-			print("Hot single 0km near you!")  
+			if position.distance_squared_to(_movement_target) < DISTANCE_THRESHOLD_SQUARED:
+				_movement_target = Vector2(
+					randf_range(0.0, 1000.0),
+					randf_range(0.0, 1000.0)
+				)
 		GuestState.GETTING_TO_DANCE_POSITION: 
 			print("Scusi, vuol ballare con me~~?")
 		GuestState.DANCING:
@@ -43,3 +59,9 @@ func _process(delta: float) -> void:
 			print("Voglio un po' di schewps, solo io e te!")
 		GuestState.DEAD:
 			print("(×_×)")
+
+
+func _physics_process(delta: float) -> void:
+	if position.distance_squared_to(_movement_target) > DISTANCE_THRESHOLD_SQUARED:
+		velocity = position.direction_to(_movement_target) * speed
+		move_and_slide()
